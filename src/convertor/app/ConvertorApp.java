2a42a9277b01305a54b95008a1c71ef31a8df3ab
JimiGitHub <jithmisachinika@gmail.com>
2020-07-19 13:06:03 +0530

    Add Options to convert

--- src/convertor/app/ConvertorApp.java
+++ src/convertor/app/ConvertorApp.java
@@ -8,6 +8,7 @@ package convertor.app;
 
 import java.io.*;
 import java.util.Properties;
+import java.util.Scanner;
 
 
 
@@ -24,19 +25,41 @@ public class ConvertorApp {
     public static void main(String[] args) throws IOException  {
        
 
-              
+            
             Properties p = new Properties();
             InputStream is = new FileInputStream("config.txt");
             p.load(is); 
+            
 
-	    File VideoMp3 = new File(p.getProperty("inputPathMp3"));
-            Mp4toMp3.ConvertMp4toMp3(VideoMp3);
+            
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Select the conversion\n 1 for Mp4 to Mp3\n 2 for Mp4 to Mkv\n 3 for Mp4 to Flv");
+            int input = scanner.nextInt();
+            
+            
+            if(input == 1){
+               File VideoMp3 = new File(p.getProperty("inputPathMp3"));
+               Mp4toMp3.ConvertMp4toMp3(VideoMp3);
+               System.out.println("Converted to Mp3");
+            }
+            if(input == 2){
+                File VideoMkv = new File(p.getProperty("inputPathMkv"));
+                Mp4toMkv.ConvertMp4toMkv(VideoMkv); 
+                System.out.println("Converted to Mkv");
+            }
+            if(input == 3){
+                File VideoFlv = new File(p.getProperty("inputPathFlv"));
+                Mp4toFlv.ConvertMp4toFlv(VideoFlv);    
+                System.out.println("Converted to FLV");
+            }
+            else{
+                System.out.println("Wrong Input");
+            }
+
                   
-            File VideoMkv = new File(p.getProperty("inputPathMkv"));
-            Mp4toMkv.ConvertMp4toMkv(VideoMkv);
 
-            File VideoFlv = new File(p.getProperty("inputPathFlv"));
-            Mp4toFlv.ConvertMp4toFlv(VideoFlv);
+
+
               
 
     }
